651768ada4e489d2a6b124cfa9e1bbf00b2a4ca5
Rahman1234562 <126483990+Rahman1234562@users.noreply.github.com>
2023-09-17 11:35:15 -0700

    updated file

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from './Card.module.css'
 
 export default function Card() {
-    const item = [
-        {id: 1, todo: "todo1"},
-        {id: 2, todo: "todo2"},
-        {id: 3, todo: "todo3"},
-        {id: 4, todo: "todo4"}
-    ]
+        const[todos, setTodos] = useState(false)
+    const handkeEdit = () => {
+
+        console.log("edit");
+        const items = [
+            {id: 1, todo: "todo1"},
+            {id: 2, todo: "todo2"},
+            {id: 3, todo: "todo3"},
+            {id: 4, todo: "todo4"}
+        ]
+       setTodos(items)
+    }
   return (
     <div>
         <div  className={classes.container}>
-                <input type="text" id="fname" name="search"/><br /> 
-
-                <button className={classes.edit}>Edit</button>
+                <input  type="text" id="fname" name="search"/><br /> 
+            
+                <button className={classes.edit} onClick={handkeEdit}>Edit</button>
                 <button>Delete</button>
         </div>
     
