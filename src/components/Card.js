f34faec64e6833891c2bcfb2f0770c54cd5c06c3
Rahman1234562 <126483990+Rahman1234562@users.noreply.github.com>
2023-09-17 10:28:56 -0700

    updated files

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from './Card.module.css'
+
+export default function Card() {
+  return (
+    <div className={classes.container}>
+         <input type="text" id="fname" name="search"/><br />
+
+    </div>
+  )
+}
